b4e26592ea9457d1bed00d9c35e3461b6d779c8b
Saugat Kandel <saugat.kandel@u.northwestern.edu>
2019-07-02 12:07:42 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ptychoSampling',
+    version='0.0.1',
+    author='Saugat Kandel',
+    author_email='saugat.kandel@u.northwestern.edu',
+    package=['ptychoSampling'],
+    scripts=[],
+    description='Ptychography simulation and phase retrieval methods',
+    requires=[
+        "numpy",
+        "scipy",
+        "skimage",
+        "matplotlib",
+        "tensorflow",
+        "tqdm",
+        "autograd"
+    ],
+)
